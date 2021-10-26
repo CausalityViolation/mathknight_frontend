688dbb28a894fa6b4f1eb38b86a6299e36e5f395
Causality <m.uddner@gmail.com>
2021-10-26 14:22:31 +0200

    add working post method (able to add students to DB via frontend)

--- src/views/teacherOverview.vue
+++ src/views/teacherOverview.vue
@@ -13,7 +13,7 @@
     </ul>
 
     <form id="loginForm">
-      <input type="text" name="username" v-model="input.username" placeholder="Student Name"/>
+      <input type="text" name="name" v-model="input.name" placeholder="Student Name"/>
       <input type="text" name="password" v-model="input.password" placeholder="Password"/>
       <input type="text" name="age" v-model="input.age" placeholder="Age"/><br><br>
       <button type="button" name="addStudent" v-on:click="postToServer"> Add Student</button>
@@ -54,11 +54,9 @@ export default {
   methods: {
 
     postToServer() {
-
       const axios = require('axios').default;
-
       axios.post('http://127.0.0.1:3030/students', {
-        studentName: this.input.username,
+        studentName: this.input.name,
         studentPassword: this.input.password,
         studentAge: this.input.age
       })
@@ -68,8 +66,24 @@ export default {
           .catch(function (error) {
             console.log(error);
           });
-    }
 
+      this.input.name = "";
+      this.input.password = "";
+      this.input.age = "";
+
+      this.getFromServer();
+
+    },
+    getFromServer() {
+      fetch('http://127.0.0.1:3030/students')
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data.students);
+            this.students = data.students;
+          });
+    }
   }
 }
 
